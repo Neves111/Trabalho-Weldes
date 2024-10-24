139538c7d7f5eb2282d0f1b66e6849137a9fadbe
Neves111 <otaviogneves11@gmail.com>
2024-10-24 17:50:50 -0300

    Matriz 1.c

---
+++ Matriz 1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+
+int matriz[3][3];
+
+int i;
+int j;
+
+for (i = 0; i < 3; i++){
+    for (j = 0; j < 3; j++){
+        scanf("%d", &matriz[i][j]);
+    }
+}
+
+for (i = 0; i < 3; i++){
+    for (j = 0; j < 3; j++){
+        printf("%d ", matriz[i][j]);
+    }
+    printf("\n");
+}
+}
