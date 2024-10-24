1d4c6b9d60cf637a4ca39047e995cc4002b1f8d7
Neves111 <otaviogneves11@gmail.com>
2024-10-24 19:18:13 -0300

    Struct 2.c

---
+++ Struct 2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+
+struct Ponto {
+    float x;
+    float y;
+};
+
+int main () {
+
+    struct Ponto ponto1;
+    
+        printf("Digite o X1: ");
+        scanf ("%f", &ponto1.x);
+        printf("Digite o Y1: ");
+        scanf ("%f", &ponto1.y);
+
+    struct Ponto ponto2;
+        printf("Digite o X2: ");
+        scanf ("%f", &ponto2.x);
+        printf("Digite o Y2: ");
+        scanf ("%f", &ponto2.y);
+
+    printf("%f", sqrt((ponto2.x - ponto1.x)*(ponto2.x - ponto1.x) + (ponto2.y - ponto1.y)*(ponto2.y - ponto1.y)));
+
+    return 0;
+}
