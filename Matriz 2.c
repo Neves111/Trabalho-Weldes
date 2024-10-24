d93075ffa5fa768d0e5e7e579b0a3283ef48925c
Neves111 <otaviogneves11@gmail.com>
2024-10-24 17:51:44 -0300

    Create Matriz 2.c

---
+++ Matriz 2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+
+int matriz[3][3];
+
+int i;
+int j;
+int soma = 0;
+
+for (i = 0; i < 3; i++){
+    for (j = 0; j < 3; j++){
+        scanf("%d", &matriz[i][j]);
+    }
+}
+
+for (i = 0; i < 3; i++){
+    for (j = 0; j < 3; j++){
+        soma += matriz [i][j];
+    }
+}
+    printf("%d", soma);
+
+    return 0;
+}
