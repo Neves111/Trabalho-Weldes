a91ff2f91199c5df79460182d3382b95f1e287b8
Neves111 <otaviogneves11@gmail.com>
2024-10-24 18:46:10 -0300

    Struct 1.c

---
+++ Struct 1.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+
+struct Livro {
+    char titulo [50];
+    char autor [50];
+    int ano;
+    char isbn [50];
+};
+
+int main () {
+
+        struct Livro livro[5];
+
+    for (int i = 0; i <= 5; i++){
+
+        printf("Titulo: ");
+        scanf ("%s", livro[i].titulo);
+
+        printf("Autor: ");
+        scanf ("%s", livro[i].autor);
+
+        printf("Ano: ");
+        scanf("%d", &livro[i].ano);
+
+        printf("ISBN: ");
+        scanf ("%s", livro[i].isbn);    
+        
+    }
+
+        for (int i = 0; i <= 5; i++){
+
+        printf("Livro %d\n", i);
+
+        printf("Titulo: %s\n", livro[i].titulo);
+        printf("Autor: %s\n", livro[i].autor);
+        printf("Anao: %d\n", livro[i].ano);
+        printf("ISBN: %s\n \n", livro[i].isbn);
+        
+    }
+
+
+    return 0;
+}
