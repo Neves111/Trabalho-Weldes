a0fa4b2d0dd59f685f714a88c1897b39bb1718db
Neves111 <otaviogneves11@gmail.com>
2024-10-24 17:53:26 -0300

    Matriz 4.c

---
+++ Matriz 4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(){
+
+int matriz[3][3] = {
+    {1, 2, 3},
+    {2, 4, 5},
+    {3, 5, 6}
+};
+int tmatriz [3][3];
+
+int i;
+int j;
+
+for (i = 0; i < 3; i++){
+    for (j = 0; j < 3; j++){
+        tmatriz[i][j] = matriz [j][i];
+    }
+}
+
+for (i = 0; i < 3; i++){
+    for (j = 0; j < 3; j++){
+        if (matriz[i][j] != tmatriz [i][j]){
+            printf("nao simetrica");
+            return 0;
+        }
+    }
+}
+printf("simetrica");
+
+return 0;
+}
