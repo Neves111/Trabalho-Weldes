19850fa95975c0d51fcd025cad18c24a98aa741f
Neves111 <otaviogneves11@gmail.com>
2024-10-24 22:24:31 -0300

    Create Struct 5.c

---
+++ Struct 5.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+struct Data {
+    int dia;
+    int mes;
+    int ano;
+};
+
+int main (){
+
+    struct Data data;
+    int final[] = {31,28,31,30,31,30,31,31,30,31,30,31};
+
+    
+
+    printf("Dia: ");
+    scanf("%d", &data.dia);
+    printf("Mes: ");
+    scanf("%d", &data.mes);
+    printf("Ano: ");
+    scanf("%d", &data.ano);
+
+    if(data.ano % 4 == 0){
+        final[2] = 29;
+    }
+
+    if(data.mes > 12){
+          return printf("Data invalida");
+    }
+
+     if (data.dia < 1 || data.dia > final[data.mes]) {
+        return printf("Data invalida");
+    }
+
+
+    printf("%d/%d/%d", data.dia, data.mes, data.ano);
+
+    return 0;
+}
