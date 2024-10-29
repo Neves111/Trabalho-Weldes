55d685017c7126c1a742fb82414f240119120df0
Neves111 <otaviogneves11@gmail.com>
2024-10-29 18:22:14 -0300

    Matriz 4.c

--- Matriz 4.c
+++ Matriz 4.c
@@ -21,8 +21,7 @@ for (i = 0; i < 3; i++){
 for (i = 0; i < 3; i++){
     for (j = 0; j < 3; j++){
         if (matriz[i][j] != tmatriz [i][j]){
-            printf("nao simetrica");
-            return 0;
+            return printf("nao simetrica");
         }
     }
 }
