105cf33270d189d315429ba3f0f867b967ca5921
Neves111 <otaviogneves11@gmail.com>
2024-11-08 16:08:22 -0300

    Ponteiro 7.c

--- Ponteiro 7.c
+++ Ponteiro 7.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
 
-int soma_matriz(int matriz[][3],int tamanho,int tamanho2){
+int soma_matriz(int *matriz, int linhas, int colunas) {
     int soma = 0;
-    int i, j;
-    for (i = 0; i < tamanho; i++){
-    for (j = 0; j < tamanho2; j++){
-        soma += matriz [i][j];
+    for (int i = 0; i < linhas; i++) {
+        for (int j = 0; j < colunas; j++) {
+            soma += *(matriz + i * colunas + j);
+        }
     }
-}
     return soma;
 }
 
-int main(){
-    int matriz[3][3] ={
-        {1,2,3},
-        {4,5,6},
-        {7,8,9}
-};
+int main() {
+    int matriz[3][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
 
-    printf("%d", soma_matriz(matriz, 3,3));
+    printf("%d\n", soma_matriz((int *)matriz, 3, 3));
 
     return 0;
-} 
+}
